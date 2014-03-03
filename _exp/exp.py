7b46f6f4d00861e4ee59cd089cabf336673b3f2d
Peipei YI <yipeipei@gmail.com>
2014-03-03 15:16:32 +0800

    add Exp to unify runtime cross machines

---
+++ _exp/exp.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+__author__ = 'Peipei YI'
+
+
+class Exp():
+    """
+    using this to unify runtime environment
+    """
+    def __init__(self, working_dir='working', task='pilot_'):
+        self.working_dir = os.path.expanduser(os.path.join('~', working_dir))
+        self.temp_dir = os.path.join(tempfile.gettempdir(), task)
+        if not os.path.isdir(self.temp_dir):
+            os.mkdir(self.temp_dir)
+
+    def __repr__(self):
+        attr_list = filter(lambda attr_name: not attr_name.startswith('_'), dir(self))
+        return '\n'.join(attr_name + '\t' + self.__dict__.get(attr_name) for attr_name in attr_list)
+
+if __name__ == '__main__':
+    a = Exp()
+    print a.__repr__()
